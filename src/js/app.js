65e584efa672034365f26ab9891bb40fb4536255
Blooming Burden <flowersoverskulls96@gmail.com>
2023-02-03 10:49:55 +0300

    Finished header mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import { isWebp } from "./modules/functions.js";
+import { clickMenu } from "./modules/functions.js";
 
 isWebp();
+clickMenu();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,4 +1,4 @@
-export function isWebp() {
+function isWebp() {
     function testWebP(callback) {
         let webP = new Image();
         webP.onload = webP.onerror = function () {
@@ -12,3 +12,22 @@ export function isWebp() {
         document.documentElement.classList.add(className);
     });
 }
+
+const clickMenu = () => {
+    const btn = document.querySelector('.main-nav__button');
+    const menu = document.querySelector('.main-nav');
+
+    const toggleMenu = (evt) => {
+        if (menu.classList.contains('main-nav--opened')) {
+            menu.classList.remove('main-nav--opened');
+            menu.classList.add('main-nav--closed');
+        } else {
+            menu.classList.remove('main-nav--closed');
+            menu.classList.add('main-nav--opened');
+        }
+    }
+
+    btn.addEventListener('click', toggleMenu);
+};
+
+export { isWebp, clickMenu }
